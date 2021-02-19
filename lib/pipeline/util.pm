e3b9046d6c425f8ea794acdf2663d9d26299ddb0
mmterpstra <m.m.terpstra.cluster@gmail.com>
2021-02-19 10:08:49 +0100

    AdFilter.pl: Adding PASS records for filtering. src/AddFragCountsToVariants.pl: New script to integrate fragment metrics from ReviewConsensusVariants to variant calls.

--- lib/pipeline/util.pm
+++ lib/pipeline/util.pm
@@ -37,7 +37,7 @@ our @EXPORT = qw(
 	_formatwalkasvcflineswithfile
 );
 
-our $VERSION = "0.8.17";
+our $VERSION = "0.8.17-2-g61df13c";
 
 # Preloaded methods go here.
 # Below is stub documentation for your module. You'd better edit it!
