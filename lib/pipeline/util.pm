789f470e7bebf3fa40ec563139e2578fd71b2e80
mmterpstra <m.m.terpstra.cluster@gmail.com>
2021-06-03 11:11:47 +0200

    Version bump and makefile update

--- lib/pipeline/util.pm
+++ lib/pipeline/util.pm
@@ -37,7 +37,7 @@ our @EXPORT = qw(
 	_formatwalkasvcflineswithfile
 );
 
-our $VERSION = "0.8.18-3-g47675d0";
+our $VERSION = "0.8.19";
 
 # Preloaded methods go here.
 # Below is stub documentation for your module. You'd better edit it!
